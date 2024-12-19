7980125b7eaae033b1af08f829d8624e075f171a
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-12-19 18:36:50 +1100

    Initial project creation

---
+++ src/test/kotlin/dev/mcarr/pgna/app/AppApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.mcarr.pgna.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
